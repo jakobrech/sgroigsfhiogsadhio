7ca41f1b5809c44c4ded38eba22d4908ab35961b
jakobrech <114768242+jakobrech@users.noreply.github.com>
2024-03-22 11:43:22 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     
+    public static void main(String args[]){
+    Node one = new Node("hi");
+    Node two = new Node("hihi");
+    Graph g = new Graph();
+    g.addNode(one);
+    g.addNode(two);
+    g.addEdge(one, two, 28);
+    System.out.println(g.getNeighbors(one));
+    }
+
 
 
 }
